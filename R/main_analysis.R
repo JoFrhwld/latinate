957011fa6c83f9596bd575d251ddde6c501f02aa
Josef Fruehwald <JoFrhwld@gmail.com>
2012-05-11 15:21:26 -0400

    a better subsetting by prefix

--- R/main_analysis.R
+++ R/main_analysis.R
@@ -1,17 +1,36 @@
 library(plyr)
 
 source("R/get_wikipedia_prefixes.R")
+## Creates objects:
+### latinate
+### prefixes
+### tables
+### theurl
+
 source("R/make_prefix_regex.R")
+## Creates objects:
+### prefix.s
+
+
+## arrange the prefixes by their length
+## longer prefixes are more restrictive searchers,
+## so they should be ordered first
+prefix.len <- nchar(prefix.s)
+prefix.order <- order(-prefix.len)
+prefix.s <- prefix.s[prefix.order]
+
 
 ## Data from English Lexicon Project
 ## http://elexicon.wustl.edu/
 words <- read.csv("data/I81379.csv")
 verbs <- subset(words, grepl("VB", POS) & !grepl(">", MorphSp) & Length >= 4)
 
+## This is a resevoir of remaining verbs
+verbs.foo <- verbs
 
-table(with(verbs, grepl(pattern=prefix.s, Word)))
 
-lverb.list <- llply(prefix.s, function(x, df){
-                                  sub <- subset(df, grepl(pattern=x, Word))
+lverb.list <- ldply(prefix.s, function(x, df){
+                                  sub <- df[grep(x, df$Word),]
+                                  verbs.foo <<- df[-grep(x, df$Word),]
                                   return(sub)
-                                }, df = verbs, .progress = "text")
+                                }, df = verbs.foo, .progress = "text")
