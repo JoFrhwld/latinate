49bbd408914480e073e215f9acc6597e91a9b7b6
Josef Fruehwald <JoFrhwld@gmail.com>
2012-05-12 08:37:48 -0400

    prefix an root frequencies

--- R/main_analysis.R
+++ R/main_analysis.R
@@ -18,6 +18,7 @@ source("R/make_prefix_regex.R")
 prefix.len <- nchar(prefix.s)
 prefix.order <- order(-prefix.len)
 prefix.s <- prefix.s[prefix.order]
+names(prefix.s) <- prefix.s
 
 
 ## Data from English Lexicon Project
@@ -26,11 +27,43 @@ words <- read.csv("data/I81379.csv")
 verbs <- subset(words, grepl("VB", POS) & !grepl(">", MorphSp) & Length >= 4)
 
 ## This is a resevoir of remaining verbs
-verbs.foo <- verbs
+assign(x = "verbs.foo", value=verbs, envir=.GlobalEnv)
 
 
-lverb.list <- ldply(prefix.s, function(x, df){
-                                  sub <- df[grep(x, df$Word),]
-                                  verbs.foo <<- df[-grep(x, df$Word),]
-                                  return(sub)
-                                }, df = verbs.foo, .progress = "text")
+lverbs.list <- vector(length = length(prefix.s), mode = "list")
+
+do_lverb_sub <- function(x, df){
+  sub <- df[grep(x, df$Word),]
+  if(nrow(sub) > 0){
+    new <- df[-grep(x, df$Word),]
+  }else{
+    new <- df
+  }
+  assign(x = "verbs.foo", 
+         value=new, 
+         envir=.GlobalEnv)
+  return(sub)
+}
+
+
+for(i in 1:length(prefix.s)){
+  x <- prefix.s[i]
+  out <- do_lverb_sub(x, df = verbs.foo)
+  if(nrow(out) > 0){
+    out$prefix <- x
+  }
+  lverbs.list[[i]] <- out
+}
+names(lverbs.list) <- prefix.s
+
+lverbs.df <- rbind.fill(lverbs.list)
+lverbs.df <- ddply(lverbs.df, .(prefix), 
+                   transform, 
+                   nprefix = length(prefix),
+                   root = gsub(prefix[1], "", Word),
+                   .progress = "text")
+
+lverbs.df <- ddply(lverbs.df, .(root), transform,
+                   nroot = length(root), .progress = "text")
+
+write.table(lverbs.df, file = "data/latinate.first.pass.txt", sep = "\t", row.names = F)
